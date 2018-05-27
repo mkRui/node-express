75da63c5d39263d2e459ca9722b9537225b960e3
shicongrui <1102163949@qq.com>
2018-05-27 20:12:17 +0800

    node 重新配置

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,10 @@ var router = express.Router();
 
 var express = require('express');
 var router = express.Router();
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send('indexss');
+  res.send('indexs');
 });
 
 module.exports = router;
