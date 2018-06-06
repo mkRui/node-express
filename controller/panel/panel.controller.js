4fdbbc4843e3cc8a7fb22a8ebf1688b2ba900b6d
shicongrui <1102163949@qq.com>
2018-06-06 23:51:56 +0800

    我的面板添加

--- controller/panel/panel.controller.js
+++ controller/panel/panel.controller.js
@@ -17,8 +17,54 @@ class panel {
    */
 
   static statistical (req, res, next) {
-    
+    articleControl.selectAllArticle().then((data) => {
+      let statisticalList = [{state: 1, totalCount: 0}, {state: 2, totalCount: 0}, {state: 3, totalCount: 0}, {state: 4, totalCount: 0}]
+      data.forEach((elem, index) => {
+        if (Number(elem.draft) === 1) {
+          statisticalList[1].totalCount += 1
+        } else if (Number(elem.draft) === 0) {
+          statisticalList[0].totalCount += 1
+        }
+        statisticalList[2].totalCount += Number(elem.articleComments)
+        statisticalList[3].totalCount += Number(elem.praise)
+      })
+      res.send(dataModel(1, '', statisticalList))
+    }).catch((data) => {
+      console.log(data)
+      res.send(dataModel(-1, '服务器忙', {}))
+    })
   }
+
+  /**
+   * @class submitArticle
+   * 
+   * @static submitArticle
+   */
+
+  static submitArticle (req, res, next) {
+    articleControl.articleSubmit().then((data) => {
+      res.send(dataModel(1, '', data[0]))
+    }).catch((data) => {
+      console.log(data)
+      res.send(dataModel(-1, '服务器忙', {}))
+    })
+  }
+
+  /**
+   * @class articleRead
+   * 
+   * @class articleRead
+   */
+
+  static articleRead (req, res, next) {
+    articleControl.articleRead().then((data) => {
+      res.send(dataModel(1, '', data[0]))
+    }).catch((data) => {
+      console.log(data)
+      res.send(dataModel(-1, '服务器忙', {}))
+    })
+  }
+
 
 }
 
