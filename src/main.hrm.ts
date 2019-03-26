58e32cd12038fe17dd7f9012db234d5872575325
jeremy <1102163949@qq.com>
2019-03-26 16:22:40 +0800

    热更新修改  文件结构确定分层

--- src/main.hrm.ts
+++ src/main.hrm.ts
@@ -5,5 +5,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
+
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close)
+  }
 }
 bootstrap();
